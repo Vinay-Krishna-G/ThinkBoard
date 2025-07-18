6b5d366406718d6884cb948d0e4d2b22eb15988d
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-07-18 11:34:34 +0530

    tailwind and daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
+//import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success("Congrats")}>Click me</button>
+    <div data-theme="night">
+      <button className="btn btn-primary">Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

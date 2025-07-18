edb65b919e4cde4c31e45ef6bdffe74b883d427e
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-07-18 10:25:07 +0530

    vite and routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,13 +9,18 @@ dotenv.config(); // Load environment variables from .env file
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter); // Apply rate limiting middleware
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1); // Exit the process if database connection fails
+  });
